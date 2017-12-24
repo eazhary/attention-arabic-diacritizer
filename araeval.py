0821beb56a9843066f7e6dab17c3be0117eaefaf
Ubuntu <ubuntu@ip-172-31-18-185.ec2.internal>
2017-12-24 12:48:17 +0000

    fixes

--- araeval.py
+++ araeval.py
@@ -21,9 +21,10 @@ def eval():
 	char2idx, idx2char = load_vocab()
 	inp = "بسم الله الرحمن الرحيم"
 	x = [char2idx[c] for c in inp+'E']
-	x += [0]*(hp.maxlen-len(inp))
+	x += [0]*(hp.maxlen-len(x))
 	x = np.array(x)
 	x = x.reshape(1,-1)
+#	x = x.repeat(hp.batch_size,axis=0)
 #	  X, Sources, Targets = X[:33], Sources[:33], Targets[:33]
 	 
 	# Start session			
@@ -38,11 +39,13 @@ def eval():
 			mname = open(hp.logdir + '/checkpoint', 'r').read().split('"')[1] # model name
 			 
 			## Inference
-			preds = np.zeros((hp.batch_size, hp.maxlen), np.int32)
+
+			#preds = np.zeros((hp.batch_size, hp.maxlen), np.int32)
+			preds = np.zeros((1, hp.maxlen), np.int32)
 			for j in range(hp.maxlen):
 				_preds = sess.run(g.preds, {g.x: x, g.y: preds})
 				preds[:, j] = _preds[:, j]
-			got = " ".join(idx2char[idx] for idx in pred).split("</S>")[0].strip()
+			got = " ".join(idx2char[idx] for idx in preds[0]).split("</S>")[0].strip()
 			print("Source: ",inp)
 			print("got : ", got)
 										  
