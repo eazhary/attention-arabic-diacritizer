a25ac8faaf249e33cb70e8f4482bddfac279ae70
eazhary <eazhary@gmail.com>
2017-12-25 10:05:40 +0200

    argmax fix

--- train.py
+++ train.py
@@ -165,7 +165,7 @@ class Graph():
 				
 			# Final linear projection
 			self.logits = tf.layers.dense(self.dec, len(char2idx))
-			self.preds = tf.to_int32(tf.argmax(self.logits, dimension=-1))
+			self.preds = tf.to_int32(tf.argmax(self.logits, axis=-1))
 			self.istarget = tf.to_float(tf.not_equal(self.y, 0))
 			self.acc = tf.reduce_sum(tf.to_float(tf.equal(self.preds, self.y))*self.istarget)/ (tf.reduce_sum(self.istarget))
 			tf.summary.scalar('acc', self.acc)
